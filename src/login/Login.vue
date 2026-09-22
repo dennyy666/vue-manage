67a0707801134191f27ef79266e9e5acda98174b
dennyy666 <2751525785@qq.com>
2026-09-22 15:44:40 +0800

    新建Login组件

---
+++ src/login/Login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <p>
+        Login works!
+    </p>
+</template>
+
+<style scoped lang="less"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../login/Login.vue'),
+    },    
+  ],
 })
 
 export default router
